1d807dd5a890a889323da87c1239061ace6cef24
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-08-06 10:52:42 -0400

    Added images into static folder and included them in components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -14,11 +14,24 @@ const App = () => (
     <Header />
     <Search />
 
-    <Categories>
+    <Categories title="Mi lista">
       <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales">
+      <Carousel>
         <CarouselItem />
       </Carousel>
     </Categories>
