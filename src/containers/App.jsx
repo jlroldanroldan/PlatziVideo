a0f2b98c2bdb074eeb4060b3aaf9da779a188b13
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-08-06 15:17:49 -0400

    Finished customising the CarouselItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -10,7 +10,11 @@ import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
 
   useEffect(() => {
     fetch("http://localhost:3000/initalState")
@@ -18,26 +22,23 @@ const App = () => {
       .then((data) => setVideos(data));
   }, []);
 
-  console.log(videos);
-
   return (
     <div className="App">
       <Header />
       <Search />
-
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
 
